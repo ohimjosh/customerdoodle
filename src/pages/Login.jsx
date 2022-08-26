c252ec0ccaa435e76913f01fa2a509db5cd3d597
Josh <joshmiranda.exe@gmail.com>
2022-08-26 14:21:13 -0400

    added design sign in + sign up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
+import logoIMG from "../assets/logo.svg";
 
 export const Login = () => {
   const { googleSignIn } = UserAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const { signIn } = UserAuth();
 
@@ -22,44 +23,78 @@ export const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('')
+    setError("");
     try {
-      await signIn(email, password)
-      navigate('/account')
+      await signIn(email, password);
+      navigate("/account");
     } catch (e) {
-      setError(e.message)
-      console.log(e.message)
+      setError(e.message);
+      console.log(e.message);
     }
   };
 
   return (
-    <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
-        <p className="py-2">
-          Don't have an account yet?{" "}
-          <Link to="/signup" className="underline">
-            Sign up.
-          </Link>
-        </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="flex flex-col justify-center place-items-center">
+        <div className="hidden sm:block">
+          <img className="h-48 w-48 whitespace-normal" src={logoIMG} alt="" />
+          <div className="font-bold text-center text-3xl">
+            <h2 className="">Support</h2>
+          </div>
+        </div>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font font-medium">Email Address</label>
-          <input onChange={(e) => setEmail(e.target.value)} className="border p-3" type="email" />
-        </div>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font font-medium">Password</label>
-          <input onChange={(e) => setPassword(e.target.value)} className="border p-3" type="password" />
-        </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign In
-        </button>
-      </form>
-      <div className="max-w[240px] m-auto py-4">
-        <GoogleButton onClick={handleGoogleSignIn} />
+
+      <div className="bg-gray-100 flex flex-col justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-[400px] w-full mx-auto bg-white p-4"
+        >
+          <h2 className="text-4xl font-bold text-center py-2">Sign In</h2>
+
+          <div className="flex flex-col py-2">
+            <label className="font-bold py-1">Email</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="border p-2"
+              type="email"
+            />
+          </div>
+          <div className="flex flex-col py-2">
+            <label className="font-bold py-1">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="border p-2"
+              type="password"
+            />
+          </div>
+          <div className="flex justify-between">
+            <p className="flex items-center">
+              <input className="mr-2" type="checkbox" />
+              Remember me for 30 days
+            </p>
+            <p>
+              <Link to="/signup" className="underline text-[#05006D]">
+                Forgot Password?
+              </Link>
+            </p>
+          </div>
+          <button className="border w-full my-5 py-2 bg-[#05006D] hover:bg-indigo-800 text-white">
+            Sign In
+          </button>
+          <div className="w-full flex place-content-center py-2">
+            <GoogleButton type="light" onClick={handleGoogleSignIn} />
+          </div>
+
+          <div className="text-center">
+            <p className="py-2">
+              Don't have an account yet?{" "}
+              <Link to="/signup" className="underline text-[#34A853]">
+                Sign up.
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
-    
   );
 };
